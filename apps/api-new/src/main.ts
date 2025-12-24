6f370aa7cb1a5b40f39c3ad9b7368f7c549fdc37
sandeshkhadka <pqrsandesh@gmail.com>
2025-12-24 23:26:56 +0545

    fix cors error

--- apps/api-new/src/main.ts
+++ apps/api-new/src/main.ts
@@ -6,7 +6,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors()
+  const allowedOrigins = process.env.CORS_ORIGINS 
+    ? process.env.CORS_ORIGINS.split(',') 
+    : ['http://localhost:3000'];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe())
 
   const config = new DocumentBuilder().setTitle("aws-ticket api")
