a95f1b186674535c02fba4f671e8df1f04d803e1
sandeshkhadka <pqrsandesh@gmail.com>
2025-12-26 14:22:37 +0545

    download ticket endpoint

--- apps/api-new/src/ticket/ticket.module.ts
+++ apps/api-new/src/ticket/ticket.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketService } from './ticket.service';
+import { TicketController } from './ticket.controller';
+import { Attendee } from 'src/attendees/entities/attendee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Attendee])],
+  controllers: [TicketController],
   providers: [TicketService],
   exports: [TicketService]
 })
